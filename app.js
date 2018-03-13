267ca7f0013d55e743cb56661f4535ae375a7ce7
Navin Bamane <navin.bamane@altdigital.in>
2018-03-13 16:28:42 +0530

    changes in app.js to fix

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ var app = http.createServer(function (req, res) {
       res.end();
     });
   } else {
-    fs.readFile(__dirname + '/public/indexs.html', function (err, data) {
+    fs.readFile(__dirname + '/public/index.html', function (err, data) {
       if (err) console.log(err);
       res.writeHead(200, {'Content-Type': 'text/html'});
       res.write(data);
